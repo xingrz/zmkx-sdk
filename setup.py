2dce43f3b266d2bb536f586ecfe3d83cc03bc1f6
XiNGRZ <hi@xingrz.me>
2023-06-14 23:08:40 +0800

    Set up package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zmkx',
+    version='0.1.0',
+    author='XiNGRZ',
+    author_email='hi@xingrz.me',
+    description='Python client for ZMKX',
+    url='https://github.com/xingrz/zmkx-sdk',
+    license='MIT',
+    packages=find_packages(),
+)
