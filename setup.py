6c0d281415b9b22e8c8c66408811521204e959c3
XiNGRZ <hi@xingrz.me>
2023-06-15 16:45:58 +0800

    0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zmkx',
-    version='0.1.0',
+    version='0.2.0',
     author='XiNGRZ',
     author_email='hi@xingrz.me',
     description='Python client for ZMKX',
